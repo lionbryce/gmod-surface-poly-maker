f74233a17e374d563e388d8f53e8d44579cfe7f0
bryce <lionbryce101@gmail.com>
2023-09-10 17:20:49 -0400

    can now handle multiple polygons

--- polymaker.lua
+++ polymaker.lua
@@ -7,40 +7,77 @@ frame:MakePopup()
 local canvas = vgui.Create("DPanel",frame)
 canvas:Dock(FILL)
 
-local points = {} -- table to hold our points
+local active_point_table = 1 -- a "pointer" to the current point table we're working with, it's just the index.
 
-local function addPoint(x,y) -- convenience function to add a point to our table
+local point_tables = {{}} -- a table to hold our arrays of points
+
+local function addPoint(x,y) -- add a point to the current table
+	local points = point_tables[active_point_table]
 	points[#points+1] = {x = x, y = y}
 end
 
-function canvas:Paint(w,h)
-	surface.SetDrawColor(255,255,255) -- might make this configurable later, for now we'll imagine we're drawing on paper
-	surface.DrawRect(0,0,w,h)
+local function removePoint() -- removes the last point from the current table
+	local points = point_tables[active_point_table]
+	points[#points] = nil
+end
 
-	draw.NoTexture()
-	surface.SetDrawColor(0,0,0)
+local function switchPointTable(index) -- switches to a new point table based on the index supplied, creates the table as necessary
+	if index < 1 then return end -- as much as I'd like to let you go below 1, we'd have to use pairs at that point which doesn't maintain order and SortedPairs is odd
 
-	local x,y = self:CursorPos()
-	addPoint(x,y) -- add the current cursor position to our table so that we can see what we're about to add
+	active_point_table = index
+	point_tables[active_point_table] = point_tables[active_point_table] or {} -- create the table if it doesn't exist
+
+	print("switched to point table",active_point_table) -- nice to know which one you're working with
+end
+
+local function drawPointsTable(points, color) -- draws a table of points, optionally with a color assumes you've already set the draw color and material if not
+	if color then
+		surface.SetDrawColor(color)
+		draw.NoTexture()
+	end
 
 	local points_len = #points
 
-	if points_len > 2 then -- you need at least 3 points to draw a polygon
+	if points_len > 2 then -- if we have more than 2 points, draw a polygon
 		surface.DrawPoly(points)
-	elseif points_len == 2 then -- make a line if we only have 2 points so we can see what we're about to add when we have less than 3 points still
+	elseif points_len == 2 then -- if we have 2 points, draw a line
 		surface.DrawLine(points[1].x,points[1].y,points[2].x,points[2].y)
-	elseif points_len == 1 then -- make a dot in case your cursor just doesn't exist anymore I guess
-		surface.DrawRect(points[1].x,points[1].y,3,3) -- 3 pixels wide and tall so you can see it even if you have the standard Windows cursor
+	elseif points_len == 1 then -- if we have 1 point, draw a dot
+		surface.DrawRect(points[1].x,points[1].y,3,3)
 	end
-
-	points[points_len] = nil -- delete the extra point we added
 end
 
-function canvas:OnMousePressed(key) -- I was today (2023-09-10) years old when I learned this function exists, almost did it with a think hook
+function canvas:Paint(w,h)
+	surface.SetDrawColor(255,255,255) -- paper background
+	surface.DrawRect(0,0,w,h)
+
+	surface.SetDrawColor(0,0,0) -- "pencil" foreground
+	draw.NoTexture()
+
+	for k,points in ipairs(point_tables) do -- draw all the point tables
+		if k == active_point_table then -- if it's the active one, draw it with red and make sure to add where the cursor is as the next point temporarily for drawing
+			local x,y = self:CursorPos()
+			addPoint(x,y)
+			drawPointsTable(points, Color(255,0,0))
+			removePoint()
+
+			surface.SetDrawColor(0,0,0) -- can't forget to switch back to black
+		else
+			drawPointsTable(points)
+		end
+	end
+end
+
+function canvas:OnMousePressed(key)
+	print(key)
 	if key == MOUSE_LEFT then -- left click = add a point
 		local x,y = self:CursorPos()
 		addPoint(x,y)
 	elseif key == MOUSE_RIGHT then -- right click = remove a point
-		points[#points] = nil
+		removePoint()
+	elseif key == MOUSE_5 then -- turns out this is the front side-button on my mouse, I will never remember that
+		switchPointTable(active_point_table + 1)
+	elseif key == MOUSE_4 then -- and this is the back side-button, they move the "table pointer" forward and backward
+		switchPointTable(active_point_table - 1)
 	end
 end
